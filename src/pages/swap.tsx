47575ba3228b82c0950bab8e5c381ec47c320e43
natnaelawel <natnael.awel@gmail.com>
2023-02-21 17:13:22 +0300

    implement swap functionality

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,9 +1,22 @@
 import clsx from "clsx";
 import HeaderCard from "../components/common/HeaderCard";
+import { useState } from "react";
 
 type Props = {};
 
 const BlackMarketPage = (props: Props) => {
+    const [swap, setSwap] = useState({
+        from: {
+            coin: "BUSD",
+            value: ""
+        },
+        to: {
+            coin: "BANDIT",
+            value: ""
+        },
+    })
+    const [toBandit, setToBandit] = useState(true)
+
     return (
         <div className="h-full relative flex flex-col items-center">
             <HeaderCard />
@@ -23,25 +36,40 @@ const BlackMarketPage = (props: Props) => {
                             </div>
                             <div className="flex items-center justify-center border-b border-pinky ">
                                 <input
-                                    type="number"
+                                    value={swap.from.value}
+                                    onChange={(e) => setSwap(prev => ({ ...prev, from: { ...prev.from, value: e.target.value } }))}
                                     placeholder=""
                                     className="h-full z-20 w-full flex-1 outline-none bg-transparent py-3 opacity-100 disabled:cursor-not-allowed"
                                 />
-                                <select className="bg-transparent outline-none">
-                                    <option className="bg-black text-white" value="BUSD">
-                                        BUSD
-                                    </option>
-                                    <option className="bg-black text-white" value="BNB">
-                                        BNB
-                                    </option>
-                                </select>
+                                {
+                                    toBandit ? (
+                                        <select value={swap.from.coin} onChange={(e) => setSwap((prev) => ({ ...prev, from: { ...prev.from, coin: e.target.value } }))} className="bg-transparent outline-none">
+                                            <option className="bg-black text-white" value="BUSD">
+                                                BUSD
+                                            </option>
+                                            <option className="bg-black text-white" value="BNB">
+                                                BNB
+                                            </option>
+                                        </select>
+                                    ) : (
+                                        <div>{swap.from.coin}</div>
+                                    )
+                                }
                             </div>
                         </div>
                         <div className="flex justify-center items-center w-full py-6">
-                            <button className="w-12 h-12 bg-pinky rounded-full cursor-pointer flex justify-center items-center">
+                            <button onClick={() => {
+                                setToBandit(prev => !prev)
+                                setSwap(prev => ({ from: prev.to, to: prev.from }))
+                            }} className="w-12 h-12 bg-pinky rounded-full cursor-pointer flex justify-center items-center">
                                 <img
                                     src="/icons/swap.svg"
-                                    className="transform transition-all duration-300 rotate-0"
+                                    className={
+                                        clsx(
+                                            "transform transition-all duration-300 rotate-0",
+                                            toBandit ? "rotate-180" : "rotate-0"
+                                        )
+                                    }
                                 />
                             </button>
                         </div>
@@ -55,15 +83,30 @@ const BlackMarketPage = (props: Props) => {
                             </div>
                             <div className="flex items-center justify-center border-b border-pinky">
                                 <input
+                                    value={swap.to.value}
+                                    onChange={(e) => setSwap(prev => ({ ...prev, to: { ...prev.to, value: e.target.value } }))}
                                     className=" h-full z-20 w-full flex-1 outline-none  bg-transparent py-3 opacity-100 disabled:cursor-not-allowed"
                                 />
-                                <div>BANDIT</div>
+                                {
+                                    toBandit ? (
+                                        <div>{swap.to.coin}</div>
+                                    ) : (
+                                        <select value={swap.to.coin} onChange={(e) => setSwap((prev) => ({ ...prev, to: { ...prev.to, coin: e.target.value } }))} className="bg-transparent outline-none">
+                                            <option className="bg-black text-white" value="BUSD">
+                                                BUSD
+                                            </option>
+                                            <option className="bg-black text-white" value="BNB">
+                                                BNB
+                                            </option>
+                                        </select>
+                                    )
+                                }
                             </div>
                         </div>
                         <div className="flex flex-col w-full text-sm py-6">
                             <div className="w-full flex justify-between">
                                 <p className="opacity-50">Price</p>
-                                <p>0 BANDIT per BNB</p>
+                                <p>0 {swap.to.coin} per {swap.from.coin}</p>
                             </div>
                             <div className="w-full flex justify-between">
                                 <p className="opacity-50">Auto Slippage</p>
@@ -71,18 +114,26 @@ const BlackMarketPage = (props: Props) => {
                             </div>
                             <div className="w-full flex justify-between">
                                 <p className="opacity-50">Minimum Received</p>
-                                <p>0.000 BANDIT</p>
+                                <p>0.000 {swap.to.coin}</p>
                             </div>
                         </div>
                         <div className="flex justify-center items-center w-full">
-                            <button className="Dashboard_button hover:opacity-100 font-medium w-3/4 py-2 rounded-sm relative z-10">
-                                Exchange
-                            </button>
+                            {
+                                toBandit ? (
+                                    <button className="Dashboard_button hover:opacity-100 font-medium w-3/4 py-2 rounded-sm relative z-10">
+                                        Approve
+                                    </button>
+                                ) : (
+                                    <button className="Dashboard_button hover:opacity-100 font-medium w-3/4 py-2 rounded-sm relative z-10">
+                                        Exchange
+                                    </button>
+                                )
+                            }
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
